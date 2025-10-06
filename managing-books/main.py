d82586091dc0f8b4f639793919a1dfa81ee71958
rahulrameshm0 <iamrahulm23@gmail.com>
2025-10-06 21:33:34 +0530

    created book managing and connected to postgres SQL

--- managing-books/main.py
+++ managing-books/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
-from typing import Annotated
+from typing import Annotated, Any
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
+from random import randint
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -41,9 +42,32 @@ async def create_book(book: Book_management, db:db_dependencies):
 async def get_all_books(db: Session = Depends(get_db)):
     books = db.query(models.Books).all()
     return books
+
 @app.get('/books/{book_id}')
 async def get_book(id: int, db:db_dependencies):
     db_post = db.query(models.Books).filter(models.Books.book_id == id).first()
     if db_post is None:
         raise HTTPException(status_code=404, detail="book id is not found")
     return db_post
+
+@app.put('/books/{book_id}')
+async def update_books(id: int, update_book: Book_management, db: db_dependencies):
+    db_book = db.query(models.Books).filter(models.Books.book_id == id).first()
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="id not found!")
+    db_book.book_name = update_book.book_name
+    db_book.author_name = update_book.author_name
+
+    db.commit()
+    db.refresh(db_book)
+
+    return {"message": "book successfully updated", "book_name":db_book.book_name, "author_name": db_book.author_name}
+
+
+@app.delete('/books/{bok_id}', status_code=status.HTTP_200_OK)
+async def delete_book(id:int, db:db_dependencies):
+    db_delete = db.query(models.Books).filter(models.Books.book_id == id).first()
+    if db_delete is None:
+        raise HTTPException(status_code=404, detail='Book id not found')
+    db.delete(db_delete)
+    return db_delete
